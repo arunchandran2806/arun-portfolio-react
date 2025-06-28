01b036a37e0d1c8ef4979952293b4b4d6a431041
arunchandran2806 <arunchandran2k4@gmail.com>
2025-06-28 10:42:28 +0530

    Updated code with recent improvements

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,10 +10,10 @@ const Contact = () => {
       transition={{duration:1.5}}
       className="my-10 text-center text-3xl">Connect with Me</motion.h2>
       <div className="text-center tracking-tighter">
-        <motion.a 
+        <motion.p 
         whileHover={{scale:1.2, transition:{duration:0.2}}}
         href={`tel:${CONTACT.phoneNo}`} 
-        className="my-4">{CONTACT.phoneNo}</motion.a>
+        className="my-4">{CONTACT.phoneNo}</motion.p>
         <motion.a 
         whileHover={{scale:1.2, transition:{duration:0.2}}} 
         href={`mailto:${CONTACT.email}`} className="border-b">{CONTACT.email}</motion.a>
